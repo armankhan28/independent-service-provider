f3dff8c70f7605cec9df7726594fab6f23c15d93
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-04-17 17:42:24 +0600

    Login section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -8,12 +9,13 @@ import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <Header></Header>
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
        <Route path='/home' element={<Home></Home>}></Route>
        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
        <Route path='*' element={<NotFound></NotFound>}></Route>
      </Routes>
      <Footer></Footer>
---
+++ src/Pages/Login/Login/Login.js
@@ -0,0 +1,76 @@
+import { async } from '@firebase/util';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Login.css'
+const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    let errorElement;
+    const [
+      signInWithEmailAndPassword,
+      user,
+      loading,
+      error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const [sendPasswordResetEmail, sending ] = useSendPasswordResetEmail(auth);
+
+    if(user){
+      navigate(from, { replace: true });
+    }
+    if (error) {
+      errorElement =  <p className="text-danger">Error: {error?.message}</p>   
+    }
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        
+        signInWithEmailAndPassword(email, password);
+    }
+
+    const navigateRegister = event =>{
+        navigate('/register');
+    }
+    const resetPassword = async() =>{
+      const email = emailRef.current.value;
+      await sendPasswordResetEmail(email);
+      toast('Sent email');
+    }
+
+  return (
+    <div className='container w-50 mx-auto mt-5'>
+      <h2 className='text-primary text-center'>Please Login</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+        </Form.Group>
+        <Button variant="primary w-50 mx-auto d-block mb-2" type="login">
+          Login
+        </Button>
+      </Form>
+      {errorElement}
+      <p>New to Car Services? <span className='register text-danger' onClick={navigateRegister}>Please Register</span></p>
+      <p>Forget Password? <span className='register text-danger' onClick={resetPassword}>Reset Password</span></p>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Login;
